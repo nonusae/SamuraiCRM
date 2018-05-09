1485387acecb1b937b206c3ec9207846f20631ad
Natthakit <nonusae@gmail.com>
2018-05-09 15:03:02 +0700

    Finish users listing in admin dashboard

--- engines/core/lib/samurai/core/engine.rb
+++ engines/core/lib/samurai/core/engine.rb
@@ -3,6 +3,8 @@ module Samurai
     class Engine < ::Rails::Engine
       isolate_namespace Samurai
 
+      paths["app/views"] << "app/views/samurai"
+
       initializer :append_migrations do |app|
         config.paths["db/migrate"].expanded.each do |p|
           app.config.paths["db/migrate"] << p
