58ae816efa11b716056092a555ee758989da20b8
Natthakit <nonusae@gmail.com>
2018-05-09 12:47:37 +0700

    Add admin view and flag to user model

--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,4 +1,7 @@
 Samurai::Core::Engine.routes.draw do
   devise_for :users, class_name: "Samurai::User", module: :devise
+  namespace :admin do
+    get '/' => 'admin#index'
+  end
   root to: "dashboard#index"
 end
